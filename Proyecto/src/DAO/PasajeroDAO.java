c2660ed410f651499d7665c0f036349edbf67256
Alejandra27 <mariaalejandraqg@gmail.com>
2016-04-06 09:46:06 -0500

    Update PasajeroDAO.java

--- Proyecto/src/DAO/PasajeroDAO.java
+++ Proyecto/src/DAO/PasajeroDAO.java
@@ -4,78 +4,108 @@ import java.awt.List;
 
 import clases.Pasajero;
 import interfaces.AerolineaDAO;
-import servidor.Controlador;
+import servidor.HiloServidor;
 
+public class PasajeroDAO implements AerolineaDAO
+{
 
+    private HiloServidor socket;
 
-public class PasajeroDAO implements AerolineaDAO{
-	
-	private Controlador socket;
+    public List pasajeros;
 
-	public List pasajeros;
+    public PasajeroDAO(HiloServidor socket)
+    {
+        this.socket = socket;
+    }
 
-	public PasajeroDAO(Controlador socket) {
-		// TODO Auto-generated constructor stub
-		this.socket = socket;
-	}
-	
-	@Override
-	public boolean insertar(String parametros[]) {
-		try {
-			Pasajero c = new Pasajero(Integer.parseInt(parametros[0]), parametros[1], parametros[2], parametros[3], Integer.parseInt(parametros[4]));
-			String sql = c.insertar();
-			socket.enviarInstruccion(sql);
-			String res = socket.darRespuestaServer();
-			if(res.equalsIgnoreCase("true"))
-			{
-				return true;
-			}else
-				return false;
-		} catch (Exception e) {
-			return false;
-		}
-	}
+    @Override
+    public boolean insertar(String parametros[])
+    {
+        try
+        {
+            Pasajero c = new Pasajero(Integer.parseInt(parametros[0]),
+                    parametros[1], parametros[2], parametros[3],
+                    Integer.parseInt(parametros[4]));
+            String sql = c.insertar();
+            socket.enviarInstruccion(sql);
+            String res = socket.darRespuestaServer();
+            if (res.equalsIgnoreCase("true"))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
 
-	@Override
-	public boolean eliminar(int PrimayKey) {
-		try {
-			Pasajero c = new Pasajero(PrimayKey);
-			String sql = c.eliminar();
-			socket.enviarInstruccion(sql);
-			String res = socket.darRespuestaServer();
-			if(res.equalsIgnoreCase("true"))
-			{
-				return true;
-			}else
-				return false;
-		} catch (Exception e) {
-			return false;
-		}
-	}
+    @Override
+    public boolean eliminar(int PrimayKey)
+    {
+        try
+        {
+            Pasajero c = new Pasajero(PrimayKey);
+            String sql = c.eliminar();
+            socket.enviarInstruccion(sql);
+            String res = socket.darRespuestaServer();
+            if (res != null)
+            {
+                if (res.equalsIgnoreCase("true"))
+                {
+                    return true;
+                }
+                else if (res.equals("null"))
+                {
+                    System.out.println("Respuesta nula ");
+                    return false;
+                }
+                else
+                    return false;
+            }
+            else
+            {
+                System.out.println("Error el valor no puede ser nulo");
+                return false;
+            }
 
-	@Override
-	public boolean actualizar() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error de " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+    }
 
-	@Override
-	public boolean consultar() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean actualizar()
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public boolean consultarPorID() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean consultar()
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public boolean consultartodos() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
+    @Override
+    public boolean consultarPorID()
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean consultartodos()
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
 }
